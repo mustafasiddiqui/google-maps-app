e08a6e802d761f12136a3e96ea3e85c80539011a
Mustafa Siddiqui <mustafasiddiqui79@gmail.com>
2020-09-06 01:08:32 -0500

    Preparing for deployment

---
+++ app.js
@@ -0,0 +1,20 @@
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var logger = require('morgan');
+var path = require('path');
+var app = express();
+
+const port = process.env.PORT || 3000;
+
+// log requests
+app.use(logger('dev'));
+
+
+// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname));
+
+app.listen(port);
+console.log(`listening on port ${port}`);
